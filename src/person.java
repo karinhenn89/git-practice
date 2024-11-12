2e86b464f56a903346818e38314ec9509a5336a6
Karin <karinhenn89@gmail.com>
2024-11-12 11:22:55 +0200

    Merge branch 'feature/merge-error'
    # Conflicts:
    #	src/Main.java

--- src/person.java
+++ src/person.java
@@ -3,8 +3,8 @@ public class person {
     public static void main(String[] args) {
 
         System.out.println("Person!");
-
-
+        int years = 2024;
+        System.out.println(years);
 
     }
 }
